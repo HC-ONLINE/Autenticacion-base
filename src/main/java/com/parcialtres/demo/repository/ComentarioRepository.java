fc1b8471233421e30f12c12f0a12b90243494ba1
lamaravilla18 <jm0162321@gmail.com>
2025-11-14 14:50:47 -0500

    repositorios

---
+++ src/main/java/com/parcialtres/demo/repository/ComentarioRepository.java
@@ -0,0 +1,17 @@
+package com.parcialtres.demo.repository;
+
+import com.parcialtres.demo.entity.Comentario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ComentarioRepository extends JpaRepository<Comentario, Long> {
+    
+    List<Comentario> findByProductoId(Long productoId);
+    
+    List<Comentario> findByUsuarioId(Long usuarioId);
+    
+    List<Comentario> findByProductoIdOrderByFechaCreacionDesc(Long productoId);
+}
